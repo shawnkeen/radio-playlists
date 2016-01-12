64b03f79152e408180cf379de037975631acf56f
skeen <skeen@galatea>
2016-01-12 17:21:29 +0100

    Add more stations and fix bugs.
    - Add Fritz.
    - Add Radio Köln.
    - Add 1Live
    - Improve detektor.fm.
    - Improve SWR3.
    - Improve overall tag fetching by getting the raw content and not the decoded text version.

--- get-playlists.py
+++ get-playlists.py
@@ -41,8 +41,8 @@ class Station:
 def getTag(url, xpathExpression, params=None):
     headers = {'User-Agent': 'curl/7.35.0'}
     page = requests.get(url, params=params, headers=headers)
-    #print page.text
-    tree = html.fromstring(page.text)
+    #print page.encoding
+    tree = html.fromstring(page.content)
     return tree.xpath(xpathExpression)
 
 
@@ -82,28 +82,34 @@ def getFM4():
     tracktitles = tree.xpath('//span[@class="tracktitle"]/text()')
     trackartists = tree.xpath('//span[@class="artist"]/text()')
 
-    trackartist = trackartists[-1]
-    tracktitle = tracktitles[-1]
-
-    return Song(trackartist, tracktitle)
+    if trackartists and tracktitles:
+        trackartist = trackartists[-1]
+        tracktitle = tracktitles[-1]
+        return Song(trackartist, tracktitle)
+    #else
+    sys.stderr.write("ERROR in fm4: "+str(tracktitles)+" "+str(trackartists)+"\n")
+    return None
 
 
 def getSWR3():
     url = 'http://www.swr3.de/musik/playlisten'
-    tags = getMultipleTags(url, ['//ul[@id="nowplaying"]/li/strong/text()', '//ul[@id="nowplaying"]/li/text()'])
-    #print tags
-    artistRaw = tags[0]
-    titleRaw = tags[1]
-    artist = None
-    title = None
-    if artistRaw and len(artistRaw) > 0:
-        artist = artistRaw[0]
-    if titleRaw and len(titleRaw) > 1:
-        title = titleRaw[1]
-    if artist and title:
-        return Song(artist, title)
+    ## The artist is encapsulated either in a <strong> or <a>, the title is the trailing rest of the same <li>.
+    ## We try both versions for the artist. First is the <strong>. If this fails, the first list is empty.
+    tags = getMultipleTags(url, ['//ul[@id="nowplaying"]/li/strong/text()',
+                                 '//ul[@id="nowplaying"]/li/a/text()',
+                                 '//ul[@id="nowplaying"]/li/text()'])
+    if tags[0]:
+        artistRaw = tags[0]
     else:
-        return None
+        artistRaw = tags[1]
+    titleRaw = tags[2]
+    if artistRaw and len(artistRaw) > 0 and titleRaw and len(titleRaw) > 1:
+        artist = artistRaw[0]
+        title = titleRaw[1]
+        return Song(artist, title)
+    #else
+    sys.stderr.write("ERROR in swr3: "+str(artistRaw)+" "+str(titleRaw)+"\n")
+    return None
 
 
 def getAntenneBayern():
@@ -138,11 +144,16 @@ def getDetektorFM():
     url = 'http://detektor.fm/'
     div = getMultipleTags(url, ['//div[@class="nowplaying nowplaying-musikstream hide white"]/strong/text()',
                                 '//div[@class="nowplaying nowplaying-musikstream hide white"]/span/text()'])
-    #print div
-    artist = div[0][0]
-    title = div[1][1]
-    #print artist, title
-    return Song(artist, title)
+    if len(div) >= 2 and isinstance(div[0], list) and isinstance(div[1], list) and div[0] and len(div[1]) >= 2:
+        artist = div[0][0]
+        title = div[1][1]
+        if '/' in title:
+            title = title.split('/')[0]
+        #print artist, title
+        return Song(artist, title)
+    #else
+    sys.stderr.write("ERROR in detektor.fm: "+str(div)+"\n")
+    return None
 
 
 def getByteFM():
@@ -154,6 +165,9 @@ def getByteFM():
     song = string.replace(song, "&ndash;", "-")
     # extract from possible html tag and split on separator
     song = deTag(song)[0].split("-")
+    if not song or len(song) < 2:
+        sys.stderr.write("ERROR in byteFM: "+str(song)+"\n")
+        return None
     artist = song[0].strip()
     title = song[1].strip()
     if title.lower().strip() == "nachrichten":
@@ -186,7 +200,50 @@ def getDonau3FM():
     return None
 
 
-#print sys.stdout.encoding
+def getFritz():
+    url = 'http://www.fritz.de/musik/playlists/index.html'
+    tag = getTag(url, '//table[@class="playlist_aktueller_tag"]')
+    if tag:
+        tag = tag[0]
+    else:
+        return None
+    title = tag.xpath('.//td[@class="tracktitle"]/text()')
+    artist = tag.xpath('.//td[@class="trackinterpret"]/a/text()')
+    if artist and title:
+        return Song(artist[-1], title[-1])
+    return None
+
+
+def getRadioKoeln():
+    url = 'http://www.radiokoeln.de/'
+    tag = getTag(url, '//div[@id="playlist_title"]')[0]
+    artist = tag.xpath('.//div/b/text()')
+    title = tag.xpath('.//div/text()')
+    #print artist, title
+    tmp = title
+    title = []
+    for item in tmp:
+        s = item.strip()
+        if s:
+            title.append(s)
+    #print title
+    if artist and title:
+        artist = artist[0]
+        title = title[-1]
+        return Song(artist, title)
+    # else
+    sys.stderr.write("ERROR in radiokoeln: "+str(artist)+" "+str(title)+"\n")
+    return None
+
+
+def get1Live():
+    url = 'http://www.einslive.de/einslive/musik/playlist/playlist284.html'
+    tag = getTag(url, '//div[@class="playlist"]')[0]
+    artist = tag.xpath('.//td/strong/text()')
+    title = tag.xpath('.//td/text()')
+    if artist and title:
+        return Song(artist[0], title[0])
+    return None
 
 
 def printPlaying(stations, lastsongs):
@@ -215,7 +272,10 @@ def main():
                 'detektor.fm': getDetektorFM,
                 'byte.fm': getByteFM,
                 'Radio7': getRadio7,
-                'Donau3FM': getDonau3FM}
+                'Donau3FM': getDonau3FM,
+                'Fritz': getFritz,
+                'RadioKoeln': getRadioKoeln,
+                '1Live': get1Live}
 
     delay = 60
     lastsongs = {}
